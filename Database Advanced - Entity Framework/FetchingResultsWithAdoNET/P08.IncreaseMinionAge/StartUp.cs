f644b5b469382aee62268c5b73c350056da70e64
kovachevmartin <martinkovachev@hotmail.com>
2019-04-19 15:57:13 +0300

    Ado.NET exercises uploaded

---
+++ Database Advanced - Entity Framework/FetchingResultsWithAdoNET/P08.IncreaseMinionAge/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using P01.InitialSetUp;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace P08.IncreaseMinionAge
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] minionsId = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            using(SqlConnection connection = new SqlConnection(Configuration.ConnectionStringToDatabase))
+            {
+                connection.Open();
+
+                string updateMinionQuery = @"UPDATE Minions
+                                                SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+                                              WHERE Id = @Id";
+
+                foreach (var id in minionsId)
+                {
+                    using(SqlCommand command = new SqlCommand(updateMinionQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                string selectMinionsQuery = "SELECT Name, Age FROM Minions";
+                using (SqlCommand command = new SqlCommand(selectMinionsQuery, connection))
+                {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[0]} {reader[1]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
