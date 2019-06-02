71f3151ba50206f40a1591a4366860bc3e444351
kovachevmartin <martinkovachev@hotmail.com>
2019-06-02 20:17:38 +0300

    folder emptied

--- Web/SIS/Apps/IRunes/IRunes.Data/DatabaseConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IRunes.Data
-{
-    public class DatabaseConfiguration
-    {
-        public const string ConnectionString =
-            @"Server=DESKTOP-OSIHADJ\SQLEXPRESS;Database=IRunesDB;Trusted_Connection=True;Integrated Security=True;";
-    }
-}
